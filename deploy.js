b7ac91e747e868fa9739c9815d0c1721a7adf668
Kevin Isom <kevin.isom@gmail.com>
2016-01-14 11:45:46 +1300

    update

--- deploy.js
+++ deploy.js
@@ -9,18 +9,20 @@ fs.readFile('./css/pure-extras.css', function (err, data) {
         throw err;
     }
     fs.writeFile('./dist/pure-extras.css', data, function (err) {
-        if (err) {
-            throw err;
-        }
-        fs.readFile('./docs/index.html', function (err, data) {
+        fs.writeFile('./dist/css/pure-extras.css', data, function (err) {
             if (err) {
                 throw err;
             }
-            fs.writeFile('./dist/index.html', data, function (err) {
+            fs.readFile('./docs/index.html', function (err, data) {
                 if (err) {
                     throw err;
                 }
-                gh.publish(path.join(__dirname, 'dist'));
+                fs.writeFile('./dist/index.html', data, function (err) {
+                    if (err) {
+                        throw err;
+                    }
+                    gh.publish(path.join(__dirname, 'dist'));
+                });
             });
         });
     });
